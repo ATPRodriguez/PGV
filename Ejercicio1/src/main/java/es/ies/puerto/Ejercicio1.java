7a1406f8c55ea93595ee9ded375e2bc050379261
ATPRodriguez <alejandrotomaspachecorodriguez@gmail.com>
2024-09-19 15:34:18 +0100

    feat: se agrega ejercicio1

---
+++ Ejercicio1/src/main/java/es/ies/puerto/Ejercicio1.java
@@ -0,0 +1,24 @@
+package es.ies.puerto;
+
+import java.io.IOException;
+
+/*
+//Escribe un programa que use ProcessBuilder para ejecutar el comando ping a una dirección IP o un dominio (por ejemplo, google.com). El programa debe capturar y mostrar la salida del proceso en la consola.
+*/
+public class Ejercicio1 {
+
+    public static final String COMMAND = "ping -c 3 google.com";
+
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder(COMMAND.split(" "));
+        try {
+            Process proceso = pb.start();
+
+            int exitCode = proceso.waitFor();
+            System.out.println("Código de salida: " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
